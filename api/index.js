cbc318a0c4800be1c5a4720d2d5850d76a574679
Maximiliano <maxigomez.udaondo@gmail.com>
2025-03-07 19:53:05 -0300

    PedidosRoute

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import ProductosApiRoutes from './routes/products.api.routes.js'
 import SesionApiRoutes from './routes/sesion.api.routes.js'
 import UsersApiRoutes from './routes/users.api.routes.js'
+import PedidosApiRoutes from './routes/pedidos.api.routes.js'
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json())
 app.use('/', ProductosApiRoutes)
 app.use('/', SesionApiRoutes)
 app.use('/', UsersApiRoutes)
+app.use('/', PedidosApiRoutes)
 
 export default app

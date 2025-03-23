0c2d6bdcf98e8d1c5aa2183648fb17674efd91f2
Maximiliano <maxigomez.udaondo@gmail.com>
2025-03-23 12:35:53 -0300

    ukukukufvffv

--- api/index.js
+++ api/index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import ProductosApiRoutes from './routes/products.api.routes.js'
 import SesionApiRoutes from './routes/sesion.api.routes.js'
 import UsersApiRoutes from './routes/users.api.routes.js'
 import PedidosApiRoutes from './routes/pedidos.api.routes.js'
@@ -12,7 +11,6 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use('/', ProductosApiRoutes)
 app.use('/', SesionApiRoutes)
 app.use('/', UsersApiRoutes)
 app.use('/', PedidosApiRoutes)
--- express.js
+++ express.js
@@ -4,8 +4,6 @@ import ProductsApiRoute from './api/routes/products.api.routes.js'
 import UserApiRoute from './api/routes/users.api.routes.js'
 import SesionApiRoute from './api/routes/sesion.api.routes.js'
 import PedidosApiRoute from './api/routes/pedidos.api.routes.js'
-import dotenv from 'dotenv';
-dotenv.config();
 
 const app = express();
 
